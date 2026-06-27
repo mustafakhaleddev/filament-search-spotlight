8cc779a94cc284c652debd78d5354894e4cc5af2
Michal Škoula <michal@skoula.com>
2026-06-27 11:23:06 +0200

    Add 'cs' i18n translation strings

---
+++ resources/lang/cs/spotlight.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'placeholder' => 'Hledat…',
+
+    'empty_state' => [
+        'prompt' => 'Začněte psát pro vyhledávání.',
+        'no_results' => 'Žádné výsledky pro „:query".',
+    ],
+
+    'keys' => [
+        'escape' => 'esc',
+    ],
+
+    'groups' => [
+        'records' => 'Záznamy',
+        'resources' => 'Zdroje',
+        'pages' => 'Stránky',
+        'actions' => 'Akce',
+        'recent' => 'Nedávné',
+        'pinned' => 'Připnuté',
+    ],
+
+    'actions' => [
+        'create' => [
+            'group' => 'Vytvořit',
+            'label' => 'Vytvořit :label',
+        ],
+    ],
+];
